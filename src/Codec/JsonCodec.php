edbfe9a0364ba6e0997160ff0d05cae0ec2ec07b
Bartosz Tomczak <bosslog@gmail.com>
2017-03-28 21:05:09 +0100

    Updated JSON codec error messages

--- src/Codec/JsonCodec.php
+++ src/Codec/JsonCodec.php
@@ -48,6 +48,30 @@ class JsonCodec implements ICodec
             case JSON_ERROR_NONE:
                 return;
                 break;
+            case JSON_ERROR_DEPTH:
+                $message = 'The maximum stack depth has been exceeded';
+                break;
+            case JSON_ERROR_STATE_MISMATCH:
+                $message = 'Invalid or malformed JSON';
+                break;
+            case JSON_ERROR_CTRL_CHAR:
+                $message = 'Control character error, possibly incorrectly encoded';
+                break;
+            case JSON_ERROR_SYNTAX:
+                $message = 'Syntax error';
+                break;
+            case JSON_ERROR_UTF8:
+                $message = 'Malformed UTF-8 characters, possibly incorrectly encoded';
+                break;
+            case JSON_ERROR_RECURSION:
+                $message = 'One or more recursive references in the value to be encoded';
+                break;
+            case JSON_ERROR_INF_OR_NAN:
+                $message = 'One or more NAN or INF values in the value to be encoded';
+                break;
+            case JSON_ERROR_UNSUPPORTED_TYPE:
+                $message = 'A value of a type that cannot be encoded was given';
+                break;
         }
 
         throw new CodecException($message);
